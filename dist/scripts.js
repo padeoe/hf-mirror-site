2663b6ed83ae9e5174f53cd011b197083bc2f19e
padeoe <padeoe@gmail.com>
2023-11-08 11:31:47 +0800

    add wechat qrcode, do not restore quick search result

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,17 +1,30 @@
-
 function searchModel() {
     const keyword = document.getElementById('searchKeyword').value.trim();
     if (keyword) {
         const domain = window.location.hostname;
         const url = `https://${domain}/models?search=${encodeURIComponent(keyword)}`;
-        window.open(url, '_blank');
+        window.location.href = url;
     }
 }
 
+let isDropdownNavigationActive = false;
 document.getElementById('searchKeyword').addEventListener('keypress', function (event) {
     if (event.keyCode === 13) {
-        event.preventDefault();  // 阻止默认的Enter键行为
-        searchModel();
+        event.preventDefault(); // Prevent the default Enter key behavior
+        if (!isDropdownNavigationActive) {
+            searchModel(); // Only call searchModel if not navigating dropdown
+        } else {
+            // Logic to trigger a click on the highlighted dropdown item
+            // You will need to keep track of which item is currently highlighted
+            // and programmatically click it.
+            // Example:
+            // document.querySelector('.search-result-item.highlighted').click();
+            const items = document.querySelectorAll('.search-result-item');
+            if (selectedIndex > -1) {
+                items[selectedIndex].click();
+            }
+        }
+        isDropdownNavigationActive = false; // Reset the flag after action
     }
 });
 let debounceTimer;
@@ -20,6 +33,7 @@ const searchResults = document.getElementById('searchResults');
 let abortController; // variable to hold the abort controller
 
 searchInput.addEventListener('input', function () {
+    selectedIndex = -1;
     clearTimeout(debounceTimer);
     debounceTimer = setTimeout(async function () {
         const query = searchInput.value.trim();
@@ -31,7 +45,7 @@ searchInput.addEventListener('input', function () {
 
             // create a new abort controller
             abortController = new AbortController();
-
+            sessionStorage.setItem('searchKeyword', query);
             try {
                 const response = await fetch(`https://hf-mirror.com/api/quicksearch?q=${encodeURIComponent(query)}&type=all`, { signal: abortController.signal });
                 const data = await response.json();
@@ -46,6 +60,7 @@ searchInput.addEventListener('input', function () {
                 });
                 searchResults.innerHTML = resultsHtml;
                 searchResults.style.display = 'block';
+                // sessionStorage.setItem('searchResults', searchResults.innerHTML);
             } catch (error) {
                 if (error.name === 'AbortError') {
                     // request was aborted, do nothing
@@ -70,13 +85,13 @@ searchInput.addEventListener('focus', function () {
 searchInput.addEventListener('blur', function () {
     setTimeout(function () {
         searchResults.style.display = 'none';
-    }, 100);
+    }, 300);
 });
 
 function openLink(url) {
-    const itemId = url.split('/').pop();
-    searchInput.value = itemId; // update the search input value
-    window.open(url, '_blank');
+    // const itemId = url.split('/').pop();
+    // searchInput.value = itemId; // update the search input value
+    window.location.href = url;
 }
 
 function copyCode(btn) {
@@ -108,11 +123,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const donateArea = document.querySelector('.groups');
+    const qrcode = document.querySelector('.groups_qrcode');
+
+    // Detect if the user is on a mobile device
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        // On mobile, show QR code on click
+        donateArea.addEventListener('click', function () {
+            qrcode.classList.toggle('show-qrcode');
+        });
+    } else {
+        // On PC, show QR code on hover is handled by CSS
+    }
+});
+
 let selectedIndex = -1; // variable to keep track of the selected item index
 
 searchInput.addEventListener('keydown', function (event) {
     const items = document.querySelectorAll('.search-result-item');
     if (event.key === 'ArrowDown') {
+        isDropdownNavigationActive = true;
         // down arrow key
         if (selectedIndex < items.length - 1) {
             selectedIndex++;
@@ -122,6 +155,7 @@ searchInput.addEventListener('keydown', function (event) {
             }
         }
     } else if (event.key === 'ArrowUp') {
+        isDropdownNavigationActive = true;
         // up arrow key
         if (selectedIndex > 0) {
             selectedIndex--;
@@ -132,12 +166,20 @@ searchInput.addEventListener('keydown', function (event) {
         // enter key
         if (selectedIndex > -1) {
             items[selectedIndex].click();
-        }
+        }        
     }
 });
 
-searchInput.addEventListener('input', function () {
-    // reset the selected index when the input value changes
-    selectedIndex = -1;
-    // ...
+
+window.addEventListener('pageshow', function() {
+    const storedKeyword = sessionStorage.getItem('searchKeyword');
+    if (storedKeyword !== null) {
+        // searchInput.focus();
+        searchInput.value = storedKeyword;
+        // const storedResults = sessionStorage.getItem('searchResults');
+        // if (storedResults) {
+        //     searchResults.innerHTML = storedResults;
+        //     searchResults.style.display = 'block';
+        // }
+    }
 });
